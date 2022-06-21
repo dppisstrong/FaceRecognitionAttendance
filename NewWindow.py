ca5b7df1f905ab16afb1f82d454c1705dab7882d
dppisstrong <93426337+dppisstrong@users.noreply.github.com>
2022-06-21 21:32:35 +0800

    通过摄像头自动采集人脸样本，生成人员表信息表
    人员信息表包括“人员ID”、“姓名”、“性别”、“图像集ID”、“手机号”等字段

--- NewWindow.py
+++ NewWindow.py
@@ -1,15 +1,90 @@
-######################################## 导入库 ################################################
+############################################# IMPORTING ################################################
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox as mess  ###消息对话框
 import cv2, os
 import csv  ###处理csv文件
 import datetime
 import time
 
 
+def assure_path_exists(path):
+    dir = os.path.dirname(path)
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+
+def check_haarcascadefile():
+    exists = os.path.isfile("haarcascade_frontalface_default.xml")
+    if exists:
+        pass
+    else:
+        mess._show(title='文件丢失', message='请联系我们获取帮助')
+        window.destroy()
+
 ######################################### 函数 ################################################
 def TakeImages():
-    pass
+    check_haarcascadefile()
+    columns = ['SERIAL NO.', '', 'ID', '', 'NAME', '', 'Tele', '', 'Sex']  #####
+    assure_path_exists("StudentDetails/")
+    assure_path_exists("TrainingImage/")
+    serial = 0
+    exists = os.path.isfile("StudentDetails\StudentDetails.csv")
+    if exists:
+        with open("StudentDetails\StudentDetails.csv", 'r') as csvFile1:
+            reader1 = csv.reader(csvFile1)
+            for l in reader1:
+                serial = serial + 1
+        serial = (serial // 2)
+        csvFile1.close()
+    else:
+        with open("StudentDetails\StudentDetails.csv", 'a+') as csvFile1:
+            writer = csv.writer(csvFile1)
+            writer.writerow(columns)
+            serial = 1
+        csvFile1.close()
+    Id = (txt.get())
+    name = (txt1.get())
+    Tele = (txt2.get())
+    Sex = (var.get())
+
+    if ((name.isalpha()) or (' ' in name)):
+        cam = cv2.VideoCapture(0)
+        harcascadePath = "haarcascade_frontalface_default.xml"
+        detector = cv2.CascadeClassifier(harcascadePath)
+        sampleNum = 0
+        while (True):
+            ret, img = cam.read()
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            faces = detector.detectMultiScale(gray, 1.3, 5)
+            for (x, y, w, h) in faces:
+                cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                # incrementing sample number
+                sampleNum = sampleNum + 1
+                # saving the captured face in the dataset folder TrainingImage
+                cv2.imwrite("TrainingImage\ " + name + "." + str(serial) + "." + Id + '.' + str(sampleNum) + ".jpg",
+                            gray[y:y + h, x:x + w])
+                # display the frame
+                cv2.imshow('Take Images', img)
+            # wait for 100 miliseconds
+            if cv2.waitKey(100) & 0xFF == ord('q'):
+                break
+            # break if the sample number is morethan 100
+            elif sampleNum > 100:
+                break
+        cam.release()
+        cv2.destroyAllWindows()
+        res = "Images Taken for ID : " + Id
+        row = [serial, '', Id, '', name, '', Tele, '', Sex]
+        with open('StudentDetails\StudentDetails.csv', 'a+') as csvFile:
+            writer = csv.writer(csvFile)
+            writer.writerow(row)
+        csvFile.close()
+        message1.configure(text=res)
+    else:
+        if (name.isalpha() == False):
+            res = "Enter correct name"
+            message.configure(text=res)  ####提示信息
 
 def psw():
     pass
