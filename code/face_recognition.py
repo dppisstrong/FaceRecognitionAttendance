51b00fd0efb1d6dc6c009593421570b23eaf6f61
YYZYQ1 <92514954+YYZYQ1@users.noreply.github.com>
2022-06-30 21:22:56 +0800

    完善人脸识别模块
    如捕捉到新的人脸，则更新显示框中的识别序号

--- code/face_recognition.py
+++ code/face_recognition.py
@@ -3,26 +3,21 @@ import cv2
 import dlib
 import keras
 
-detector = dlib.get_frontal_face_detector()
-
+detector = dlib.get_frontal_face_detector() #构建人脸框位置检测器
 
 class Recognition:
     def __init__(self):
         self.model = None
         self.face_id_list = []
         self.my_name = ''
-        self.get_name_flag = True
-        self.delay_flag = True
-        self.delay_frame = 0
         self.font = cv2.FONT_HERSHEY_SIMPLEX
 
     def face_recognition(self, model_path, stream):
         self.load_model(model_path)
-        frame = 0
         while stream.isOpened():
             ret, img_camera = stream.read()
             img_gray = cv2.cvtColor(img_camera, cv2.COLOR_BGR2GRAY)
-            faces = detector(img_gray, 1)   # 上采样一次，原分辨率尺寸扩大一倍
+            faces = detector(img_gray, 1)
             if len(faces) != 0:
                 for i, face in enumerate(faces):
                     left = face.left()
@@ -34,25 +29,18 @@ class Recognition:
                     if img_cut.shape[0] != 0 and img_cut.shape[1] != 0:
                         after_resize = cv2.resize(img_cut, (64, 64))
                         cv2.rectangle(img_camera, (left, top), (right, bottom), (0, 255, 0), 2)
-                        faceID = self.face_predict(after_resize)
-                        if len(self.face_id_list) <= 20:
-                            self.face_id_list.append(faceID)
-
-            if frame > 10:
-                max_label = max(self.face_id_list, key=self.face_id_list.count)
-                img_set_id = "{}".format(max_label)
-                if self.get_name_flag:
-                    self.my_name = str(img_set_id)
-                    self.get_name_flag = False
-                cv2.putText(img_camera, self.my_name, (left, top - 10), self.font, 1,
-                            (0, 255, 0), 1)
-                if (cv2.waitKey(1) == ord('q')):
-                    break
-
+                        faceID = self.face_predict(after_resize)  # 得到此帧人脸对应ID
+                        if len(self.face_id_list) < 20:
+                            self.face_id_list.append(faceID)  # 将预测的人脸ID加入到列表当中
+                        else:
+                            print(self.face_id_list)
+                            max_label = max(self.face_id_list, key=self.face_id_list.count)
+                            img_set_id = "{}".format(max_label)
+                            self.my_name = str(img_set_id)
+                            self.face_id_list = []
+            cv2.putText(img_camera, self.my_name, (left, top - 10), self.font, 1, (0, 255, 0), 1)
             cv2.imshow("camera", img_camera)
-            frame += 1
-            k = cv2.waitKey(10)
-            if k == 27:
+            if (cv2.waitKey(1) == ord('q')):
                 break
 
     def load_model(self, model_path):
@@ -72,7 +60,6 @@ class Recognition:
         self.face_recognition(model_path, cap)
         cap.release()
         cv2.destroyAllWindows()
-
 
 if __name__ == '__main__':
     predict = Recognition()
